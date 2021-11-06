c69f7a20bcbdc1a6b7c02e51cf737431aa735c65
Olivier <olivierdabl@Oliviers-MBP.home>
2021-11-06 18:29:48 +0100

    last things

--- sync_folders.sh
+++ sync_folders.sh
@@ -1,1 +1,5 @@
-rsync -aurhvi --delete-during --progress --bwlimit=5000 src/ dst/
+src="/Volumes/Elements1TB/Photography/Photos/"
+dst="/Users/olivierdabl/pCloud Drive/Photography/Backups/Live"
+
+mkdir -p "$dst"
+rsync -aurhvi --delete --progress --bwlimit=5000 "$src" "$dst"
